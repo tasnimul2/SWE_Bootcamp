42fa4526aef85482d6b8abf417736bc52f759964
tasnimul2 <chowdhury1478@gmail.com>
2021-01-20 18:13:58 -0500

    day 2 week three

--- Week Three/Day 1/script.js
+++ Week Three/Day 1/script.js
@@ -10,4 +10,3 @@ for(let x = 0; x < 10; x++){
 
 console.log(typeof 'c')
 console.log(typeof "wod")
-
---
+++ Week Three/Day 2/script.js
@@ -0,0 +1,5 @@
+let celcius = 20;
+
+let fahrenheit = (celcius * 9/5 + 32);
+
+console.log(celcius + " degree celcius is " + fahrenheit + " degrees fahrenheit");
