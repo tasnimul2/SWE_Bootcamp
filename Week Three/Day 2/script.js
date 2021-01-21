f5d8e99de9fa66bb9caefecb5cc769718705528d
tasnimul2 <chowdhury1478@gmail.com>
2021-01-20 20:40:35 -0500

    finished classwork

--- Week Three/Day 2/script.js
+++ Week Three/Day 2/script.js
@@ -3,3 +3,23 @@ let celcius = 20;
 let fahrenheit = (celcius * 9/5 + 32);
 
 console.log(celcius + " degree celcius is " + fahrenheit + " degrees fahrenheit");
+
+
+fahrenheit = 50;
+celcius = (fahrenheit - 32) * 5/9 ;
+console.log(celcius + " degree celcius is " + fahrenheit + " degrees fahrenheit");
+
+
+//class activity - Knicks vs nets;
+
+let kicksAvg = (109 + 95 + 123)/3;
+let netsAvg = (97 + 112 + 101)/3;
+
+
+if(kicksAvg> netsAvg){
+    console.log("Knicks win");
+}else if(kicksAvg === netsAvg){
+    console.log("tie");
+}else{
+    console.log("nets win");
+}
