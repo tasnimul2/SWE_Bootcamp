6e02552a03cd07524a5c3f893c9f74b9a923d63a
tasnimul2 <chowdhury1478@gmail.com>
2021-01-23 12:01:55 -0500

    JS random, objects, arrays etc

--- Week Three/Day 3/script.js
+++ Week Three/Day 3/script.js
@@ -28,9 +28,69 @@ console.log(arrMethid);
 const john ={
     firstName: 'john',
     lastName: 'williams',
-    age:'2020-1996',
-    friends:['me','my self', 'I']
+    age:24,
+    friends:['me','my self', 'I'],
+    calBirthYear: function (){
+        this.birthYear = 2020 - this.age;
+        return this.birthYear;
+    }
 }
 
 console.log(john);
-john['instagram'] = '@john';
+john['instagram'] = '@john'; //adds instagram to the object.
+
+console.log(john['age']);
+console.log(john.calBirthYear());
+
+
+/*
+Let's go back to Lucas and John comparing their BMIs! This time, let's use objects to implement the calculations! 
+Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+ 
+1. For each of them, create an object with properties for their full name, mass, and height (Lucas Miller and John Smith)
+
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). 
+Store the BMI value to a property, and also return it from the method.
+
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. 
+Example: "John Smith's BMI (28.3) is higher than Lucas Miller's (23.9)!"
+ 
+TEST DATA: Lucas weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+*/
+
+const johnB = {
+    weight: 92,
+    height: 1.95,
+    getBMI: function (){
+        return this.weight/this.height ** 2;
+    }
+}
+
+const lucas ={
+    weight: 78,
+    height: 1.69,
+    getBMI: function (){
+        return this.weight/this.height**2;
+    }
+}
+
+
+console.log("lucas BMI: "+ lucas.getBMI() + " John BMI : "+ johnB.getBMI() );
+if(lucas.getBMI() > johnB.getBMI() ){
+    console.log("Lucas has heigher BMI");
+}else if(lucas.getBMI() < johnB.getBMI()){
+    console.log("john has heigher BMI");
+}else {
+    console.log("they have samw BMI");
+}
+
+
+
+for(let i = 0; i < 100; i++){
+    let dice = Math.trunc((Math.random()*100))+1;
+    console.log(dice);
+    
+}
+
+let dice = Math.trunc((Math.random()*100))+1;
+
