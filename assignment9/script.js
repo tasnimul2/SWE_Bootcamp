dbdc89d160c5baaec9ab805fb3d8b0739308039a
tasnimul2 <chowdhury1478@gmail.com>
2021-01-25 17:21:06 -0500

    assignment 9 done

--- assignment9/script.js
+++ assignment9/script.js
@@ -16,4 +16,16 @@ const john ={
 
 console.log( "john has " + john.friends.length + " friends, and his best friends is called "+ john.friends[2]);
 console.log( john.getSummary());
+const btn1 = document.querySelector('.open-modal-btn1');
+const btn2 = document.querySelector('.open-modal-btn2');
+const btn3 = document.querySelector('.open-modal-btn3');
+const btn4 = document.querySelector('.close-modal-btn');
+const openBtn = document.querySelector('.open-modal-btn');
+btn1.href = '#open-window';
+btn2.href = '#open-window';
+btn3.href = '#open-window';
+btn4.href = '#main-window';
 
+openBtn.addEventListener('click', function(){
+
+})
